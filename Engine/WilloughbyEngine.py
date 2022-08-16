be54b8b8d900b3f988f71747c9eb40cf0a8f0a93
Shriya Dhaundiyal <110204529+shriyadh@users.noreply.github.com>
2022-08-16 14:26:29 -0400

    Add files via upload

---
+++ Engine/WilloughbyEngine.py
@@ -0,0 +1,10 @@
+from Engine import Engine
+
+
+def WilloughbyEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        return self.cuurent_mileage - self.last_service_mileage > 60000
